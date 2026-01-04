340e527095dc3a70036f9bd5c4df44c560a67987
Slipuk <ustyianovychstepan1@gmail.com>
2026-01-04 18:48:43 +0200

    allow to show the temperature of selected city

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const API_KEY = "e01c177d898972fce98c1957371f3969"
+const API_KEY = "e97520a1021db8e017ed0ed6f6373da0";
+
+const showWeatherButton = document.getElementById("showWeatherButton");
+const cityInput = document.getElementById("cityInput");
+const weatherDiv = document.getElementById("weather");
 
 async function getWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=en`;
@@ -11,7 +15,21 @@ async function getWeather(city) {
 
     const data = await response.json();
     console.log(data);
+    return data;
 }
 
-getWeather("London");
+async function showWeather() {
+    try {
+        const cityName = cityInput.value;
+        const data = await getWeather(cityName);
+
+        renderWeatherDivText(data);
+    } catch (error) {
+        weatherDiv.innerHTML = error.message;
+    }
+}
+
+function renderWeatherDivText(data) {
+    weatherDiv.innerText = `${data.main.temp}°C`;
+}
 
